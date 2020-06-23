952b5b095c8224f4e7cab3d16bef2e5314607b0b
Гаврилов Артем <dede13@yandex.ru>
2020-06-23 15:00:55 +0300

    Работа с Атаном для 2Д.

---
+++ GameDevTalk/Assets/Scripts/TestAtan/PlayerAtan.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAtan : MonoBehaviour
+{
+    public bool RotateY;
+
+    void Update()
+    {
+        var positionMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var scale = transform.localScale;
+        if (positionMouse.x < transform.position.x)
+        {
+            if (scale.x > 0)
+            {
+                scale.x *= -1;
+                if(RotateY) scale.y *= -1;
+                transform.localScale = scale;
+            }
+        }
+        else
+        {
+            if (scale.x < 0)
+            {
+                scale.x *= -1;
+                if(RotateY) scale.y *= -1;
+                transform.localScale = scale;
+            }
+        }
+    }
+}
---
+++ GameDevTalk/Assets/Scripts/TestAtan/ShotgunAtan.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotgunAtan : MonoBehaviour
+{
+    void Update()
+    {
+        var positionMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var rotationVector = transform.rotation.eulerAngles;
+        var direction = positionMouse - transform.position;
+        rotationVector.z = Mathf.Atan2(direction.y, direction.x) * 180.0f / 3.14f;
+        transform.rotation = Quaternion.Euler(rotationVector);
+    }
+}
